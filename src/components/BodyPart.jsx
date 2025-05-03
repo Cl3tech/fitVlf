e7a310a852d8cc6aabcdc5440d2e5b132f326439
Luis Paradela <luisparadela@outlook.fr>
2025-05-03 12:02:10 +0100

    rapid api integration

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -12,12 +12,11 @@ const BodyPart = ({ item, setBodyPart, bodyPart}) => {
         className="bodyPart-card"
         sx={{
             borderTop: bodyPart === item ? '4px solid #ff2625' : '',
-                borderTop: '4px solid #ff2625',
-                background: '#fff',
-                borderBottomLeftRadius: '20px',
-                width: '270px',
-                height: '280px',
-                cursor:'pointer', gap: '47px'}}
+            background: '#fff',
+            borderBottomLeftRadius: '20px',
+            width: '270px',
+            height: '280px',
+            cursor:'pointer', gap: '47px'}}
     >
         <img src={Icon} alt="ginasio" styles={{ width: '40px', height: '40px'}}/>
     </Stack>
--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,30 +1,29 @@
 import React from 'react';
-import {Box, Stack, Typography, Button} from '@mui/material';
-import HeroBannerImage from '../assets/images/banner.png'
-
+import { Box, Typography, Button } from '@mui/material';
+import HeroBannerImage from '../assets/images/banner.png';
 
 const HeroBanner = () => {
   return (
     <Box sx={{
-      mt: { lg:'212px', xs: '70px'},
-      ml: { sm: '50px'}
+      mt: { lg: '212px', xs: '70px' },
+      ml: { sm: '50px' }
     }} position="relative" p="20px">
-      <Typography color="#3268a8" 
-      fontSize="26px"
-      fontWeight="600" >
+      <Typography color="#3268a8"
+        fontSize="26px"
+        fontWeight="600" >
         Vila Flor Fitness
       </Typography>
-      <Typography fontWeight={700} sx={{fontSize: {lg: '44px' , xs: '40px'}}} mb="20px" mt="30px">
+      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="20px" mt="30px">
         Suar, Sorrir <br /> e Repetir
       </Typography>
       <Typography fontSize="22px" lineHeight="35px" mb={4}>
         Descobre os melhores exercicios para ti...
       </Typography>
-      <Button variant="contained" color='secondary' href='#exercises' sx={{ backgroundColor:'#3268a8', padding:'10px' }} >Explorar Exercicos</Button>
-      <Typography fontWeight={600} color="primary" 
-        sx={{ 
+      <Button variant="contained" color='secondary' href='#exercises' sx={{ backgroundColor: '#3268a8', padding: '10px' }} >Explorar Exercicios</Button>
+      <Typography fontWeight={600} color="primary"
+        sx={{
           opacity: 0.2,
-          display: { lg: 'block', xs:'none' }
+          display: { lg: 'block', xs: 'none' }
         }}
         fontSize='180px'
       >
@@ -35,4 +34,4 @@ const HeroBanner = () => {
   )
 }
 
-export default HeroBanner
+export default HeroBanner;
